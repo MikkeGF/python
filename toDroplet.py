8e56ea86532e5c421c9c28990d9cdef64cf2a26d
MikkeGF <n9pemi00students.oamk.fi>
2020-12-01 23:57:21 +0200

    toDroplet scripti

--- toDroplet.py
+++ toDroplet.py
@@ -1,21 +1,17 @@
-from paramiko import SSHClient
-import getpass
-from scp import SCPClient
+import os
 
-remote_path = input('Remotepath?')
-local_path = input('Local file path?')
-pswd = getpass.getpass('Password?')
+# input use
+
+# local_path = input('local path?')
+# remote_path = input('Remote path?')
+
+# paths saved to variable
+local_path = '/home/mikkegf/Development/schoolsite/build/*'
+remote_path = '/var/www/mikkegf.codes'
+
 
 REMOTE_HOST = '207.154.197.252'
 REMOTE_USER = 'mikke'
-REMOTE_PASS = pswd
-
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(REMOTE_HOST, username=REMOTE_USER, password=REMOTE_PASS)
 
 
-scp = SCPClient(ssh.get_transport())
-scp.put(local_path, remote_path, recursive=True)
-scp.close()
-
+os.system(f"scpgit+
